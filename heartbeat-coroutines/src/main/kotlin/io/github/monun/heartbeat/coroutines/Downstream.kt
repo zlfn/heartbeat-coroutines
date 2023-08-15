89a87b44f9d51bf9ffabab919acf5b0fa43eab86
zlfn <elusive1102@naver.com>
2023-08-15 18:34:19 +0900

    :boom: Bukkit API를 이용해 Plugin을 가져오게 변경

--- heartbeat-coroutines/src/main/kotlin/io/github/monun/heartbeat/coroutines/Downstream.kt
+++ heartbeat-coroutines/src/main/kotlin/io/github/monun/heartbeat/coroutines/Downstream.kt
@@ -2,30 +2,19 @@ package io.github.monun.heartbeat.coroutines
 
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
-import org.bukkit.plugin.java.PluginClassLoader
+import org.bukkit.plugin.java.JavaPlugin
 
 
 /**
  * Bukkit의 LibraryLoader로 로딩되었을 때 ClassLoader의 플러그인을 불러옵니다.
  */
 internal object Downstream {
-    private val classLoaderFields
-        get() = PluginClassLoader::class.java.declaredFields.filter {
-            ClassLoader::class.java.isAssignableFrom(it.type)
-        }.onEach { field ->
-            field.isAccessible = true
-        }
-
-    private val PluginClassLoader.internalLoaders: List<ClassLoader>
-        get() = classLoaderFields.map { it.get(this) }.filterIsInstance<ClassLoader>()
-
     fun pullPlugin(): Plugin {
-        val classLoader = Downstream::class.java.classLoader
-
-        return Bukkit.getPluginManager().plugins.find { plugin ->
-            val pluginClassLoader = plugin.javaClass.classLoader as PluginClassLoader?
-
-            pluginClassLoader != null && (pluginClassLoader === classLoader || pluginClassLoader.internalLoaders.any { it === classLoader })
-        } ?: error("heartbeat-coroutine must be loaded from PluginClassLoader")
+        return try {
+            Bukkit.getLogger().info(JavaPlugin.getProvidingPlugin(Downstream::class.java).name)
+            JavaPlugin.getProvidingPlugin(Downstream::class.java)
+        } catch(e: Exception) {
+            error("Failed to load plugin")
+        }
     }
 }
